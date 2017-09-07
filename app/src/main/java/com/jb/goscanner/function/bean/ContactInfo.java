9239c663d2a1805d87696d18aebd41f5f4abf83c
yueeeeliu <yueeeeliu@163.com>
2017-09-07 09:47:15 +0800

    bean类更改

--- app/src/main/java/com/jb/goscanner/function/bean/ContactInfo.java
+++ app/src/main/java/com/jb/goscanner/function/bean/ContactInfo.java
@@ -2,25 +2,49 @@ package com.jb.goscanner.function.bean;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by liuyue on 2017/9/3.
  */
 
 public class ContactInfo implements Serializable {
+    private String id;
     private String name;
-    private String phoneNum;
-    private String skype;
-    private String facebook;
+    private String remark;
+    private String imgUrl;
+    private ArrayList<DetailItem> mPhone;
+    private ArrayList<DetailItem> mEmail;
+    private ArrayList<DetailItem> mWechat;
+    private ArrayList<DetailItem> mOther;
+
+
 
     public ContactInfo() {
+        mPhone = new ArrayList<>();
+        mEmail = new ArrayList<>();
+        mWechat = new ArrayList<>();
+        mOther = new ArrayList<>();
     }
 
-    public ContactInfo(String name, String phoneNum, String skype, String facebook) {
+
+    public ContactInfo(String id, String name, String remark, String imgUrl, ArrayList<DetailItem> phone, ArrayList<DetailItem> email, ArrayList<DetailItem> wechat, ArrayList<DetailItem> other) {
+        this.id = id;
         this.name = name;
-        this.phoneNum = phoneNum;
-        this.skype = skype;
-        this.facebook = facebook;
+        this.remark = remark;
+        this.imgUrl = imgUrl;
+        mPhone = phone;
+        mEmail = email;
+        mWechat = wechat;
+        mOther = other;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -31,47 +55,127 @@ public class ContactInfo implements Serializable {
         this.name = name;
     }
 
-    public String getPhoneNum() {
-        return phoneNum;
+    public String getRemark() {
+        return remark;
     }
 
-    public void setPhoneNum(String phoneNum) {
-        this.phoneNum = phoneNum;
+    public void setRemark(String remark) {
+        this.remark = remark;
     }
 
-    public String getSkype() {
-        return skype;
+    public String getImgUrl() {
+        return imgUrl;
     }
 
-    public void setSkype(String skype) {
-        this.skype = skype;
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
     }
 
-    public String getFacebook() {
-        return facebook;
+    public ArrayList<DetailItem> getPhone() {
+        return mPhone;
     }
 
-    public void setFacebook(String facebook) {
-        this.facebook = facebook;
+    public void setPhone(ArrayList<DetailItem> phone) {
+        mPhone = phone;
     }
 
-    @Override
-    public String toString() {
-        return "ContactInfo{" +
-                "name='" + name + '\'' +
-                ", phoneNum='" + phoneNum + '\'' +
-                ", skype='" + skype + '\'' +
-                ", facebook='" + facebook + '\'' +
-                '}';
+    public ArrayList<DetailItem> getEmail() {
+        return mEmail;
     }
 
-    public ArrayList<DetailItem> parseToDetailItems() {
-        ArrayList<DetailItem> itemList = new ArrayList();
-        itemList.add(new DetailItem("Name", name));
-        itemList.add(new DetailItem("PhoneNum", phoneNum));
-        itemList.add(new DetailItem("Wechat", skype));
-        itemList.add(new DetailItem("QQ", facebook));
+    public void setEmail(ArrayList<DetailItem> email) {
+        mEmail = email;
+    }
 
-        return itemList;
+    public ArrayList<DetailItem> getWechat() {
+        return mWechat;
+    }
+
+    public void setWechat(ArrayList<DetailItem> wechat) {
+        mWechat = wechat;
+    }
+
+    public ArrayList<DetailItem> getOther() {
+        return mOther;
+    }
+
+    public void setOther(ArrayList<DetailItem> other) {
+        mOther = other;
+    }
+
+    public void setPhone(DetailItem detailItem) {
+        mPhone.add(detailItem);
+    }
+
+    public void setEmail(DetailItem detailItem) {
+        mEmail.add(detailItem);
+    }
+
+    public void setWechat(DetailItem detailItem) {
+        mWechat.add(detailItem);
+    }
+
+    public void setOther(DetailItem detailItem) {
+        mOther.add(detailItem);
+    }
+
+    // 将ContactInfo解析成ArrayList<DetailItem>用于RecyclerView展示
+    public static ArrayList<DetailItem> parseToDetailItem(ContactInfo info) {
+        ArrayList<DetailItem> items = new ArrayList<>();
+
+        // 注意头部内容的放置
+        DetailItem head = new DetailItem();
+        head.setId(info.getId());
+        head.setGroup(info.getRemark());
+        head.setTag(info.getName());
+        head.setValue(info.getImgUrl());
+        head.setContactId(DetailItem.GROUP_HEAD); // 这是标志
+        items.add(head);
+
+        items.add(new DetailItem(DetailItem.GROUP_PHONE)); // 这是分组（没有实际内容）
+        items.addAll(info.getPhone());  // 实际详情内容
+        items.add(new DetailItem(DetailItem.GROUP_EMAIL));
+        items.addAll(info.getEmail());
+        items.add(new DetailItem(DetailItem.GROUP_WECHAT));
+        items.addAll(info.getWechat());
+        items.add(new DetailItem(DetailItem.GROUP_OTHER));
+        items.addAll(info.getOther());
+        return items;
+    }
+
+    // 用于将RecyclerView的内容组装成ContactInfo
+    public ContactInfo combineToContactInfo(List<DetailItem> items) {
+        ContactInfo info = new ContactInfo();
+        int i = 0;
+        if (items.get(i).getContactId().equals(DetailItem.GROUP_HEAD)) {
+            DetailItem head = items.get(0);
+            info.setId(head.getId());
+            info.setRemark(head.getGroup());
+            info.setImgUrl(head.getValue());
+            info.setName(head.getTag());
+            i++;
+        }
+        for (DetailItem item : items) {
+            if (item.getValue() == null || item.getGroup().equals(DetailItem.GROUP_HEAD)) {
+                break;
+            }
+
+            item.setContactId(this.id);
+            if (item.getGroup().equals(DetailItem.GROUP_EMAIL)) {
+                info.setEmail(item);
+            }
+            if (item.getGroup().equals(DetailItem.GROUP_WECHAT)) {
+                info.setWechat(item);
+            }
+            if (item.getGroup().equals(DetailItem.GROUP_OTHER)) {
+                info.setOther(item);
+            }
+            if (item.getGroup().equals(DetailItem.GROUP_PHONE)) {
+                info.setPhone(item);
+            }
+        }
+        return info;
     }
 }
+
+
--- app/src/main/java/com/jb/goscanner/function/bean/DetailItem.java
+++ app/src/main/java/com/jb/goscanner/function/bean/DetailItem.java
@@ -7,15 +7,38 @@ import java.io.Serializable;
  */
 
 public class DetailItem implements Serializable{
+    private String id; // 主键
+    private String contactId; // 外键
     private String tag;
-    private String value;
+    private String value; // value为null只有一种情况：记录的内容是分组
+    private String group; // 分组：phone／email/wechat/...
 
+    public static final String GROUP_PHONE = "PHONE";
+    public static final String GROUP_EMAIL = "EMAIL";
+    public static final String GROUP_WECHAT = "WECHATE";
+    public static final String GROUP_OTHER = "OTHER";
+    public static final String GROUP_HEAD = "HEAD";
     public DetailItem() {
     }
 
-    public DetailItem(String tag, String value) {
+    public DetailItem(String group) {
+        this.group = group;
+    }
+
+    public DetailItem(String id, String tag, String value, String group, String contactId) {
+        this.id = id;
         this.tag = tag;
         this.value = value;
+        this.group = group;
+        this.contactId = contactId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getTag() {
@@ -34,11 +57,19 @@ public class DetailItem implements Serializable{
         this.value = value;
     }
 
-    @Override
-    public String toString() {
-        return "DetailItem{" +
-                "tag='" + tag + '\'' +
-                ", value='" + value + '\'' +
-                '}';
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(String contactId) {
+        this.contactId = contactId;
     }
 }
