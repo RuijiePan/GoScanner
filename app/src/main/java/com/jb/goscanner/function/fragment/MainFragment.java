c9e0087522307dfb4c17d4436dc8023b01b8952c
JiePier <zquprj@gmail.com>
2017-09-11 02:57:06 +0800

    完善代码。。。。

--- app/src/main/java/com/jb/goscanner/function/fragment/MainFragment.java
+++ app/src/main/java/com/jb/goscanner/function/fragment/MainFragment.java
@@ -12,6 +12,7 @@ import android.widget.ImageView;
 import com.google.zxing.activity.CaptureActivity;
 import com.jb.goscanner.R;
 import com.jb.goscanner.base.fragment.BaseFragment;
+import com.jb.goscanner.function.activity.ContactActivity;
 import com.jb.goscanner.function.activity.RecordDetailActivity;
 import com.jb.goscanner.function.bean.ContactInfo;
 import com.jb.goscanner.function.widget.BottomTab;
@@ -31,6 +32,9 @@ public class MainFragment extends BaseFragment implements View.OnClickListener {
     private View mWebPage;
     private View mTextContent;
     private View mOtherView;
+    private int mIndex;
+    public static final int GENERATE = 0;
+    public static final int COLLECTION = 1;
 
     @Nullable
     @Override
@@ -53,7 +57,7 @@ public class MainFragment extends BaseFragment implements View.OnClickListener {
     }
 
     private void initData() {
-
+        mIndex = GENERATE;
     }
 
     private void setListener() {
@@ -65,7 +69,7 @@ public class MainFragment extends BaseFragment implements View.OnClickListener {
         mBottomTab.setTabClickListener(new BottomTab.OnTabClickListener() {
             @Override
             public void onClickGenerate() {
-
+                mIndex = GENERATE;
             }
 
             @Override
@@ -76,6 +80,7 @@ public class MainFragment extends BaseFragment implements View.OnClickListener {
 
             @Override
             public void onClickCollection() {
+                mIndex = COLLECTION;
                 //Loger.w("ruijie", "click collection");
             }
         });
@@ -85,16 +90,34 @@ public class MainFragment extends BaseFragment implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.main_business_card:
-                RecordDetailActivity.startRecordDetailActivity(mContext, new ContactInfo(), RecordDetailActivity.MODE_EDITABLE);
+                if (mIndex == GENERATE) {
+                    RecordDetailActivity.startRecordDetailActivity(mContext, new ContactInfo(), RecordDetailActivity.MODE_EDITABLE);
+                } else {
+                    Intent intent = new Intent(mContext, ContactActivity.class);
+                    startActivity(intent);
+                }
                 break;
             case R.id.main_home_web:
+                goTo();
                 break;
             case R.id.main_text_content:
+                goTo();
                 break;
             case R.id.main_other:
+                goTo();
                 break;
             case R.id.main_top_person_icon:
+                goTo();
                 break;
+        }
+    }
+
+    public void goTo() {
+        if (mIndex == GENERATE) {
+
+        } else {
+            Intent intent = new Intent(mContext, ContactActivity.class);
+            startActivity(intent);
         }
     }
 }
